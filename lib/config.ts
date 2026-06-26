6af4f6cbe9e3eb0c049f41c12bfc85969881824b
dacarpoc <dacarpoc@gmail.com>
2026-06-26 11:22:36 +0200

    fix: syntax error in config.ts after cleanup

--- lib/config.ts
+++ lib/config.ts
@@ -28,10 +28,7 @@ export const ESPARRAGOS_CONFIG = {
   mapsUrl: "https://maps.google.com/?q=41.672234,-4.720406",
 };
 
-  mapsEmbed:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3000!2d-4.4167!3d41.2833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sC.+Calvario+8+Remondo+Segovia!5e0!3m2!1ses!2ses!4v1",
-  mapsUrl: "https://maps.google.com/?q=C.+Calvario,+8,+40216+Remondo,+Segovia",
-};
+
 
 export const ESPARRAGOS_ENVIOS = {
   dias: ["Lunes", "Martes", "Miércoles"],
